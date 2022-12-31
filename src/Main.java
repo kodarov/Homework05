10e2ee671828459738c4d1ccd75b97bc66ea7264
kodarov_salavat <kodarov@gmail.com>
2022-12-31 12:32:40 +0400

    HomeWork04\Lesson05

--- src/Main.java
+++ src/Main.java
@@ -54,5 +54,22 @@ public class Main {
     }
     public static void task5() {
         System.out.println("Задача 5");
+        int monthNumber = 12;
+        switch (monthNumber){
+            case 1: System.out.println(monthNumber + " месяц принадлежит сезону зима"); break;
+            case 2: System.out.println(monthNumber + " месяц принадлежит сезону зима"); break;
+            case 3: System.out.println(monthNumber + " месяц принадлежит сезону весна"); break;
+            case 4: System.out.println(monthNumber + " месяц принадлежит сезону весна"); break;
+            case 5: System.out.println(monthNumber + " месяц принадлежит сезону весна"); break;
+            case 6: System.out.println(monthNumber + " месяц принадлежит сезону лето"); break;
+            case 7: System.out.println(monthNumber + " месяц принадлежит сезону лето"); break;
+            case 8: System.out.println(monthNumber + " месяц принадлежит сезону лето"); break;
+            case 9: System.out.println(monthNumber + " месяц принадлежит сезону осень"); break;
+            case 10: System.out.println(monthNumber + " месяц принадлежит сезону осень"); break;
+            case 11: System.out.println(monthNumber + " месяц принадлежит сезону осень"); break;
+            case 12: System.out.println(monthNumber + " месяц принадлежит сезону зима"); break;
+            default:
+                System.out.println("ошибка"); break;
+        }
     }
 }
